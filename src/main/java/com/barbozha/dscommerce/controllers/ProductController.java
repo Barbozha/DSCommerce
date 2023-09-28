51c7de48e4c9dcaaf2766bdf138df09406386235
Barbozha <barbozha@gmail.com>
2023-09-28 12:16:29 -0300

    Incluindo o ResponseEntity no Controller - décimo primeiro commit

--- src/main/java/com/barbozha/dscommerce/controllers/ProductController.java
+++ src/main/java/com/barbozha/dscommerce/controllers/ProductController.java
@@ -1,14 +1,18 @@
 package com.barbozha.dscommerce.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.barbozha.dscommerce.dto.ProductDTO;
 import com.barbozha.dscommerce.services.ProductService;
@@ -21,23 +25,27 @@ public class ProductController {
 	private ProductService service;
 	
 	@GetMapping(value = "/{id}")
-	public ProductDTO findById(@PathVariable Long id) {
-		return service.findById(id);
-		//ProductDTO dto = service.findById(id);
-		//return dto;
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+		//return service.findById(id);
+		ProductDTO dto = service.findById(id);
+		return ResponseEntity.ok(dto);
 	}
 	
 	@GetMapping
 	//import org.springframework.data.domain.Page;
 	//import o org.springframework.data.domain.Pageable;
-	public Page<ProductDTO> findAll(Pageable pageable){
-		return service.findAll(pageable);
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+		//return service.findAll(pageable);
+		Page<ProductDTO> dto = service.findAll(pageable);
+		return ResponseEntity.ok(dto);
 	}
 	
 	//Inserindo dados no banco de dados
 	@PostMapping
-	public ProductDTO insert(@RequestBody ProductDTO dto) {
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
 		dto = service.insert(dto);
-		return dto;
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
