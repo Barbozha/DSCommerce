a7a14e2cac2ab7d2952109a66776f066d3aaed01
Barbozha <barbozha@gmail.com>
2023-10-03 11:24:01 -0300

    Inclusão da rotina delete - décimo terceiro commit

--- src/main/java/com/barbozha/dscommerce/controllers/ProductController.java
+++ src/main/java/com/barbozha/dscommerce/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ public class ProductController {
 	@Autowired
 	private ProductService service;
 	
+	//Buscando os dados por id
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		//return service.findById(id);
@@ -50,9 +52,17 @@ public class ProductController {
 		return ResponseEntity.created(uri).body(dto);
 	}
 	
+	//Atualizando dados no banco de dados
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);
+	}
+	
+	//Deletando um registro no banco de dados
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/barbozha/dscommerce/services/ProductService.java
+++ src/main/java/com/barbozha/dscommerce/services/ProductService.java
@@ -13,25 +13,25 @@ import com.barbozha.dscommerce.repositories.ProductRepository;
 @Service
 
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Transactional(readOnly = true) // para não dar lock no banco de dados
 	public ProductDTO findById(Long id) {
 		Product product = productRepository.findById(id).get();
 		return new ProductDTO(product);
 	}
-	
+
 	@Transactional(readOnly = true)
-	//import org.springframework.data.domain.Pageable;
-	//import org.springframework.data.domain.Page;
-	//Será paginada os resultados
+	// import org.springframework.data.domain.Pageable;
+	// import org.springframework.data.domain.Page;
+	// Será paginada os resultados
 	public Page<ProductDTO> findAll(Pageable pageable) {
 		Page<Product> prod = productRepository.findAll(pageable);
 		return prod.map(x -> new ProductDTO(x));
 	}
-	
+
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
@@ -39,7 +39,7 @@ public class ProductService {
 		entity = productRepository.save(entity);
 		return new ProductDTO(entity);
 	}
-	
+
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) {
 		Product entity = productRepository.getReferenceById(id);
@@ -48,13 +48,16 @@ public class ProductService {
 		return new ProductDTO(entity);
 	}
 
+	@Transactional
+	public void delete(Long id) {
+		productRepository.deleteById(id);
+	}
+
 	private void copyDtoToEntity(ProductDTO dto, Product entity) {
-		
 		entity.setName(dto.getName());
 		entity.setDescription(dto.getDescription());
 		entity.setPrice(dto.getPrice());
 		entity.setImgUrl(dto.getImgUrl());
 	}
-	
-	
+
 }
